0f5dbc0d3309f275067db2fb22bcdc7bd6a9ca87
marshmallowyoshi <97353790+marshmallowyoshi@users.noreply.github.com>
2023-08-08 10:32:54 +0100

    plot title modulus error

--- compare all.py
+++ compare all.py
@@ -15,14 +15,14 @@ y = np.asarray(df.iloc[:,3])
 
 
 allSegments, polynomialRegression = alg.newFullSegmentAnalysis(x,y, name=name, peakRateThreshold=0.03, join=True, segmentMode='poly', plot=False)
-x_2 = alg.reduceDataDecimate(x, 2)
-y_2 = alg.reduceDataDecimate(y, 4)
+x_2 = alg.reduceDataDecimate(x, 8)
+y_2 = alg.reduceDataDecimate(y, 8)
 allSegments2, polynomialRegression2 = alg.newFullSegmentAnalysis(x_2,y_2, name=name, peakRateThreshold=0.03, join=True, segmentMode='poly', plot=False)
 area = alg.integralDifference(polynomialRegression[0][0], polynomialRegression2[0][0], (allSegments[0][0],allSegments[0][2]))
 allAreas = alg.allSegmentsIntegral(allSegments, allSegments2, polynomialRegression, polynomialRegression2)
 
 totalDiff = 100 * np.asarray(allAreas)[:,1].sum()/np.asarray(allAreas)[:,2].sum()
-print(totalDiff)
+print(np.round(totalDiff,2), "%", sep="")
 
 # numPoints = 100
 # for i in range(len(allSegments)):
@@ -41,4 +41,7 @@ print(totalDiff)
 
 alg.cubicPlot(allSegments, polynomialRegression)
 alg.cubicPlot(allSegments2, polynomialRegression2, color='blue')
+title = "Modulus Error: " + str(np.round(totalDiff,2)) + "%"
+
+plt.title(title)
 plt.show()
